8ab5dd95f076560153802dbf74e91d96428f6562
Harsha Vardhan <kpharsha@yahoo.com>
2024-01-31 21:53:32 +0530

    go time chnages

--- 06time/main.go
+++ 06time/main.go
@@ -11,4 +11,11 @@ func main() {
 	presentTime := time.Now()
 	fmt.Println(presentTime)
 
+	fmt.Println(presentTime.Format("01-02-2006 15:04:05 Monday"))
+
+	createdDate := time.Date(2020, time.August, 14, 23, 23, 0, 0, time.UTC)
+
+	fmt.Println(createdDate)
+	fmt.Println(createdDate.Format("01-02-2006 Monday"))
+
 }
